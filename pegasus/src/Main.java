52019c2dd420f290d6561f7cd9892e1754dcb2d7
Kübra Kılıç <100305722+klckubra@users.noreply.github.com>
2022-12-25 19:07:17 +0300

    update main

--- pegasus/src/Main.java
+++ pegasus/src/Main.java
@@ -1,5 +1,13 @@
+import java.util.Scanner;
 
 public class Main {
 
-	
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.println("************Hoş Geldiniz. Bizi tercih ettiğiniz için teşekkür ederiz.************\n");
+		
+		Package abc = new Package();
+		Package.Bagaj def = abc.new Bagaj();
+	}
 }
